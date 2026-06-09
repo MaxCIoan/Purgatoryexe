7ddf046e71735b81e863f4293a19566d2c430ed4
MaxC <119760322+MaxCIoan@users.noreply.github.com>
2026-06-09 16:58:09 +0200

    Add files via upload

--- db/index.ts
+++ db/index.ts
@@ -1,7 +1,6 @@
 import { getDatabase } from "@netlify/database";
 
-const database = getDatabase();
-
 export function query(text: string, params: unknown[] = []) {
+  const database = getDatabase();
   return database.pool.query(text, params);
 }
